0024c8a849a40f212f1b6876c7be5d18cdeed951
RooBarsic <35538949+RooBarsic@users.noreply.github.com>
2018-10-01 10:29:21 +0300

    Update Newton.java

--- semestr3/vichMat/lab1/Newton.java
+++ semestr3/vichMat/lab1/Newton.java
@@ -8,16 +8,26 @@ public class Newton extends FindingRootAlgorithm{
 
     @Override
     protected double findRoot(double left, double right, double eps) {
+
+
         double a = polynom.getKoef(3) * 3.;
         double b = polynom.getKoef(2) * 2.;
         double c = polynom.getKoef(1);
         PolynomX polynomX2 = new PolynomX(2, new double[]{c, b, a});
-        root = left;
+
+        double a2 = a * 2;
+        double b2 = b * 1;
+        PolynomX polynomX1 =  new PolynomX(1, new double[]{b, a});
+
+
+        if(polynom.getFx(left) * polynomX1.getFx(left) > 0) root = left;
+        else if(polynom.getFx(right) * polynomX1.getFx(right) > 0) root = right;
+        else root = (left + right) / 2.0;
+
         while (true){
             numberOfIteration++;
-            //-2,7x^3 - 1,48x^2 + 19,23x + 6,35
-
             double root2 = root - (polynom.getFx(root) / polynomX2.getFx(root));
+            if((root2 < left) || (right < root2)) root2 = (left + right) / 2.0;
             if(abs(root - root2) < eps){
                 root = root2;
                 break;
