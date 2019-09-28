d48507c717b76d4132b1f8f4c88d07add5489bd7
Farrukh Karimov <35538949+RooBarsic@users.noreply.github.com>
2019-09-28 10:41:07 +0300

    Update task1.sh

--- semestr5/OSP/lab2/task1.sh
+++ semestr5/OSP/lab2/task1.sh
@@ -2,7 +2,19 @@
 IFS="";
 if [ "$1" != "" ]
 then
-	getent group $1
+	usersInfo=$(getent group $1)
+	colonsNum=0
+	for (( i=0; i<${#usersInfo}; i++ )); do
+	  if [ $colonsNum == 3 ]
+	  then
+	  	echo "${usersInfo:$i}"
+	  	break
+	  fi
+	  if [ "${usersInfo:$i:1}" == ":" ]
+	  then
+	  	colonsNum=$(($colonsNum+1))
+	  fi
+	done
 else 
 	echo "No argument passed. Please try again!"
 fi
