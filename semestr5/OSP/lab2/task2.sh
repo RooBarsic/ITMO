d0628ee0abc3cd8e999260e248093811b6626972
Farrukh Karimov <35538949+RooBarsic@users.noreply.github.com>
2019-10-09 22:25:07 +0300

    Update task2.sh

--- semestr5/OSP/lab2/task2.sh
+++ semestr5/OSP/lab2/task2.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 if [ "$1" != "" ]
 then
-   lsOut=$(ls)
-   for file in $lsOut; do 
-   	  va1=$(ls -l $file)
-   	  flag=0
-   	  link=""
-   	  last=""
-   	  for var in $va1; do
-   	  	if [ "$flag" == 1 ]
-   	  	then
-
-   	  		if [ "$var" == "$1" ]
-   	  		then
-   	  			echo "$link"
-   	  		fi
-   	    fi
-   	    if [ "$var" == "->" ]
-   	  	then
-   	  		flag=1
-   	  		link="$last"
-   	  	fi
-   	  	last="$var"
-   	  done
-   done
+  lsOut=$(ls -t)
+  for file in $lsOut; do 
+    va1=$(ls -l $file)
+    flag=0
+    link=""
+    last=""
+    for var in $va1; do
+      if [ "$flag" == 1 ]
+        then
+        if [ "$var" == "$1" ]
+    	  then
+    	    echo "$link"
+    	fi
+      fi
+      if [ "$var" == "->" ]
+    	then
+    	  flag=1
+    	  link="$last"
+      fi
+      last="$var"
+    done
+  done
 else 
-	echo "No argument passed. Please try again!"
+  echo "No argument passed. Please try again!"
 fi
 exit 0
 
